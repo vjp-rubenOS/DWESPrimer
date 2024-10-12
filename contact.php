36036382df2f3ee0ac3ea64a87afd05c52893c19
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-12 13:51:06 +0200

    errores se muestra datos bien no

--- contact.php
+++ contact.php
@@ -1,8 +1,54 @@
 <?php
-require 'views/contact.view.php';
+
+$errores=[];
+$nombre='';
+$apellido='';
+$subject='';
+$texto='';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Guardamos los datos del formulario con trim para quitar espacios al principio y final
+    // y usamos htmlspecialchars para "saltar" los comando que puedan meter de html
+
+    $nombre=isset($_POST['nombre'])?htmlspecialchars(trim($_POST['nombre'])): '';
+    $apellido=isset($_POST['apellido'])?htmlspecialchars(trim($_POST['apellido'])): '';
+    $email=isset($_POST['email'])?htmlspecialchars(trim($_POST['email'])): '';
+    $subject=isset($_POST['subject'])?htmlspecialchars(trim($_POST['subject'])): '';
+    $texto=isset($_POST['texto'])?htmlspecialchars(trim($_POST['texto'])): '';
+
+    // validamos los campos que son obligatorios
+    // Campo nombre
+    if(empty($nombre)){
+        $errores[]="El campo First Name no puede estar vacio";
+    }
+    //Campo email, que no solo validamos si esta vacion sino que tenga el formato adecuado
+    if(empty($email)){
+        $errores[]=" El campo Email no puede estar vacio";
+    }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $errores[]="Email incorrecto";
+    }
+
+    //Campo asunto
+    if(empty($subject)){
+        $errores[]="El campo subject no puede estar vacio";
+    }
+    // Si no hya errores mostramos la informacion de los campos
+    if(empty($errores)){
+        echo "<div class='alert alert-info'>";
+        echo "<h1>Informacion enviada</h1><br>";
+        echo "Nombre: $nombre<br>";
+        echo "Apellido: $apellido<br>";
+        echo "Correo $email<br>";
+        echo "Asunto: $subject<br>";
+        echo "Mensaje: $texto<br>";
+        echo "</div>";
+
+        // Borramos los valores que habia en los campos y los ponemos vacios
+        $nombre = $apellido = $email = $subject = $texto = '';
+    }
+
     
 }
+require 'views/contact.view.php';
 
 ?>
--- views/contact.view.php
+++ views/contact.view.php
@@ -3,94 +3,103 @@ include __DIR__.'/partials/inicio-doc.part.php';
 ?>
 
 <!-- Navigation Bar -->
-   <nav class="navbar navbar-fixed-top navbar-default">
-     <div class="container">
-	   	 <div class="navbar-header">
-	   	 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-	   	 		<span class="sr-only">Toggle navigation</span>
-	   	 		<span class="icon-bar"></span>
-	   	 		<span class="icon-bar"></span>
-	   	 		<span class="icon-bar"></span>
-	   	 	</button>
-            <a  class="navbar-brand page-scroll" href="#page-top">
-              <span>[PHOTO]</span>
-            </a>
-	   	 </div>
-	   	 <div class="collapse navbar-collapse navbar-right" id="menu">
-	   	 	<ul class="nav navbar-nav">
-	   	 		<li class=" lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
-	   	 		<li class="lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-	   	 		<li class="lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-	   	 		<li class="active"><a href="#"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
-	   	 	</ul>
-	   	 </div>
-   	 </div>
-   </nav>
+<nav class="navbar navbar-fixed-top navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand page-scroll" href="#page-top">
+        <span>[PHOTO]</span>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse navbar-right" id="menu">
+      <ul class="nav navbar-nav">
+        <li class="lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
+        <li class="lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
+        <li class="lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+        <li class="active"><a href="#"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <!-- End of Navigation Bar -->
 
 <!-- Principal Content Start -->
-   <div id="contact">
-   	  <div class="container">
-   	    <div class="col-xs-12 col-sm-8 col-sm-push-2">
-       	   <h1>CONTACT US</h1>
-       	   <hr>
-       	   <p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-	       <form class="form-horizontal">
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-6">
-	       	  	    <label class="label-control">First Name</label>
-	       	  		<input class="form-control" type="text" name="nombre">
-	       	  	</div>
-	       	  	<div class="col-xs-6">
-	       	  	    <label class="label-control">Last Name</label>
-	       	  		<input class="form-control" type="text" name="apellido">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Email</label>
-	       	  		<input class="form-control" type="text" name="email">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Subject</label>
-	       	  		<input class="form-control" type="text" name="subject">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Message</label>
-	       	  		<textarea class="form-control" name="texto">Escribe aqui </textarea>
-	       	  		<button class="pull-right btn btn-lg sr-button">SEND</button>
-	       	  	</div>
-	       	  </div>
-	       </form>
-	       <hr class="divider">
-	       <div class="address">
-	           <h3>GET IN TOUCH</h3>
-	           <hr>
-	           <p>Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero.</p>
-		       <div class="ending text-center">
-			        <ul class="list-inline social-buttons">
-			            <li><a href="#"><i class="fa fa-facebook sr-icons"></i></a>
-			            </li>
-			            <li><a href="#"><i class="fa fa-twitter sr-icons"></i></a>
-			            </li>
-			            <li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a>
-			            </li>
-			        </ul>
-				    <ul class="list-inline contact">
-				       <li class="footer-number"><i class="fa fa-phone sr-icons"></i>  (00228)92229954 </li>
-				       <li><i class="fa fa-envelope sr-icons"></i>  kouvenceslas93@gmail.com</li>
-				    </ul>
-				    <p>Photography Fanatic Template &copy; 2017</p>
-		       </div>
-	       </div>
-	    </div>   
-   	  </div>
-   </div>
-<!-- Principal Content Start -->
+<div id="contact">
+  <div class="container">
+    <div class="col-xs-12 col-sm-8 col-sm-push-2">
+      <h1>CONTACT US</h1>
+      <hr>
+      <p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+
+      <!-- Mostrar errores si los hay  Este lo he modificado yo -->
+      <?php if (!empty($errores)): ?>
+      <div class="alert alert-danger">
+        <ul>
+          <?php foreach ($errores as $error): ?>
+          <li><?php echo $error; ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+      <?php endif; ?>
+
+      <form class="form-horizontal" method="POST" action="contact.php">
+        <div class="form-group">
+          <div class="col-xs-6">
+            <label class="label-control">First Name</label>
+            <input class="form-control" type="text" name="nombre" value="<?php echo htmlspecialchars($nombre); ?>">
+          </div>
+          <div class="col-xs-6">
+            <label class="label-control">Last Name</label>
+            <input class="form-control" type="text" name="apellido" value="<?php echo htmlspecialchars($apellido); ?>">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-xs-12">
+            <label class="label-control">Email</label>
+            <input class="form-control" type="text" name="email" value="<?php echo htmlspecialchars($email); ?>">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-xs-12">
+            <label class="label-control">Subject</label>
+            <input class="form-control" type="text" name="subject" value="<?php echo htmlspecialchars($subject); ?>">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-xs-12">
+            <label class="label-control">Message</label>
+            <textarea class="form-control" name="texto"><?php echo htmlspecialchars($texto); ?></textarea>
+            <button class="pull-right btn btn-lg sr-button">SEND</button>
+          </div>
+        </div>
+      </form>
+      <hr class="divider">
+      <div class="address">
+        <h3>GET IN TOUCH</h3>
+        <hr>
+        <p>Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero.</p>
+        <div class="ending text-center">
+          <ul class="list-inline social-buttons">
+            <li><a href="#"><i class="fa fa-facebook sr-icons"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter sr-icons"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a></li>
+          </ul>
+          <ul class="list-inline contact">
+            <li class="footer-number"><i class="fa fa-phone sr-icons"></i> (00228)92229954 </li>
+            <li><i class="fa fa-envelope sr-icons"></i> kouvenceslas93@gmail.com</li>
+          </ul>
+          <p>Photography Fanatic Template &copy; 2017</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Principal Content End -->
 
 <?php
 include __DIR__.'/partials/fin-doc.part.php';
