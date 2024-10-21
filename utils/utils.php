3028ba1a40ef926c5454395ab9443be570733e73
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-21 11:28:43 +0200

    fin proyecto 1

--- utils/utils.php
+++ utils/utils.php
@@ -4,19 +4,21 @@
 // Funcion que determina si la opcion del menu actual esta activa 
 // Recibe por parametro un string que es la URL de una de las opciones del menu
 
-function esOpcionMenuActiva(string $opcionMenu): bool {
+function esOpcionMenuActiva(string $opcionMenu): bool
+{
     // Compara la URL actual de la pagina ($_SERVER['REQUEST_URI']) con la opción del menu
     // Si coinciden retorna true, lo que indica que esta activa.
     return $_SERVER['REQUEST_URI'] === $opcionMenu;
 }
 
-// Función para verificar si alguna de las opciones en un array está activa
-// Recibe un array que contiene varias rutas de opciones del menú.
-function existeOpcionMenuActivaEnArray(array $opcionesMenu): bool {
-    // Recorre cada opción del array.
+// Función para verificar si alguna de las opciones del array esta activa
+// Recibe un array que contiene varias rutas de opciones del menu.
+function existeOpcionMenuActivaEnArray(array $opcionesMenu): bool
+{
+    // Recorre el array.
     foreach ($opcionesMenu as $opcion) {
-        // Para cada opción, llama a la función 'esOpcionMenuActiva' para verificar si está activa.
-        // Si alguna de las opciones está activa, retorna true inmediatamente.
+        // Para cada opción, llama a la funcion creada antes 'esOpcionMenuActiva' para verificar si esta activa.
+        // Si alguna de las opciones está activa, retorna true.
         if (esOpcionMenuActiva($opcion)) {
             return true;
         }
@@ -24,5 +26,3 @@ function existeOpcionMenuActivaEnArray(array $opcionesMenu): bool {
     // Si ninguna de las opciones del array está activa, retorna false.
     return false;
 }
-
-?>
