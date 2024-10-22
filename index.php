ccf0fbeb6a80f994b572e76a39e18ea6b6a32d59
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-22 20:05:16 +0200

    bucle index.php

--- index.php
+++ index.php
@@ -3,7 +3,14 @@ require 'utils/utils.php'; // utils antes que view
 require 'entities/ImagenGaleria.class.php';
 require 'entities/ImagenGaleria.class.php';
 
+$imagenes=[];
 
+for($i=0;$i<=12;$i++){
+    $imagenGaleria= new ImagenGaleria($i.'.jpg','imagen'.$i,rand()*5000,rand()*5000,rand()*5000);
+    $imagenes[]=$imagenGaleria;
+
+   
+}
 require 'views/index.view.php';
 
 ?>
