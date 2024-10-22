ae77dd359179d24f538c7db164c5bd974863b550
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-22 19:43:03 +0200

    requires index

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require 'utils/utils.php'; // utils antes que view
+require 'entities/ImagenGaleria.class.php';
+require 'entities/ImagenGaleria.class.php';
+
+
 require 'views/index.view.php';
 
 ?>
