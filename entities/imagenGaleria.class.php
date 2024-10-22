70078f9cfbbe9c861aacfce51fccea775724e948
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-22 19:14:08 +0200

    clase imagenGaleria

---
+++ entities/imagenGaleria.class.php
@@ -0,0 +1,29 @@
+<?php
+class imagenGaleria{
+    /**
+     * @var string
+     */
+    private $nombre;
+     /**
+     * @var string
+     */
+    
+    private $descripcion;
+     /**
+     * @var int
+     */
+
+    private $numVisualizaciones;
+     /**
+     * @var int
+     */
+
+    private $numLikes;
+    
+     /**
+     * @var int
+     */
+    private $numDownloads;
+}
+
+?>
