eaa82e40691159df42a476ba1c77963be6e149ae
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-30 19:15:36 +0100

    string.php creado

---
+++ utils/strings.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$errorStrings[UPLOAD_ERR_OK]="No hay ningun error.";
+$errorStrings[UPLOAD_ERR_INI_SIZE]="El fichero es demasiado grande";
+$errorStrings[UPLOAD_ERR_FORM_SIZE]="El fichero es demasiado grande";
+$errorStrings[UPLOAD_ERR_PARTIAL]="No se ha podido subir el fichero";
+$errorStrings[UPLOAD_ERR_NO_FILE]="No se encuentra ningun archivo";
+$errorStrings[UPLOAD_ERR_NO_TMP_DIR]="No existe el directorio temporal";
+$errorStrings[UPLOAD_ERR_CANT_WRITE]="No se puede escribir";
+$errorStrings[UPLOAD_ERR_EXTENSION]="No se ha podido mover el archivo de desatino";
+
+define('ERROR_STRINGS',$errorStrings);
+
+?>
