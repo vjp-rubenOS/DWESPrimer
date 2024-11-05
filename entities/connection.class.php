fb0384464ce088bce2142c64f2e9008435dbc9be
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-11-05 18:53:20 +0100

    clase connection

---
+++ entities/connection.class.php
@@ -0,0 +1,18 @@
+<?php
+    class Connection{
+        public static function make(){
+            $option=[
+                PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAME utf8',
+                PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_PERSISTENT=>true
+            ];
+            try{
+                $connection= new PDO('mysql:host=dwes.local;dbname=proyecto;charset=utf8','user','user',$option);
+            }catch(PDOException $error){
+                die($error->getMessage());
+            }
+            return $connection; 
+        }
+        
+    }
+?>
